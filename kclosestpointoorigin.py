a476033a64be2f14143bb5292106dbd22f5a6acb
SzymonJawors <jawor12325@gmail.com>
2026-05-05 14:20:34 +0200

    k closest point

---
+++ kclosestpointoorigin.py
@@ -0,0 +1,15 @@
+import heapq
+
+def kClosest(points, k):
+    min_heap = []
+    for x, y in points:
+        dist = (x**2) + (y**2)
+        min_heap.append((dist, x, y))
+        
+    heapq.heapify(min_heap)
+    res = []
+    for _ in range(k):
+        dist,x,y = heapq.heappop(min_heap)
+        res.append([x,y])
+    
+    return res
