e41981deade32b0734444b20ad38ff19977195e0
SzymonJawors <jawor12325@gmail.com>
2026-05-10 09:43:39 +0200

    crsschedule2

---
+++ courseschedule2.py
@@ -0,0 +1,30 @@
+def findOrder(numCourses, prerequisites):
+    preMap = {i: [] for i in range(numCourses)}
+    
+    for crs, pre in prerequisites:
+        preMap[crs].append(pre)
+        
+    res = []
+    
+    visit, cycle = set(), set()
+    
+    def dfs(crs):
+        if crs in cycle:
+            return False
+        if crs in visit:
+            return True
+
+        cycle.add(crs)
+        for pre in preMap[crs]:
+            if not dfs(pre):
+                return False
+        
+        cycle.remove(crs)
+        visit.add(crs)
+        res.append(crs)
+        return True
+    
+    for i in range(numCourses):
+        if not dfs(i):
+            return []
+    return res
